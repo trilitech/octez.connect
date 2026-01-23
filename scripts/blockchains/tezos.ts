98db7880aadf479e51c59ffb22781bbd1e0d85a9
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2026-01-23 17:16:55 +0100

    chore(tezos): adjust tezlink shadownet identifier

--- scripts/blockchains/tezos.ts
+++ scripts/blockchains/tezos.ts
@@ -24,7 +24,7 @@ export enum NetworkType {
   SEOULNET = 'seoulnet',
   SHADOWNET = 'shadownet',
   TALLINNNET = 'tallinnnet',
-  TEZLINK_SHADOWNET = 'tezlink_shadownet',
+  TEZLINK_SHADOWNET = 'tezlink-shadownet',
   CUSTOM = 'custom'
 }
 
